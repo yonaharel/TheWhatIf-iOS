3dc8e4791524f7994633b1bba98b00f700189c36
Yona Harel <yonah@balink.net>
2022-05-28 00:20:36 +0300

    Initial Commit

---
+++ The What If/The_What_IfApp.swift
@@ -0,0 +1,20 @@
+//
+//  The_What_IfApp.swift
+//  The What If
+//
+//  Created by Yona Harel on 28/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct The_What_IfApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
